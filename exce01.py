4636fd1683e27b68d57d7ea999446a18598b9d12
Marcos Camargo <mcmpgui12@gmail.com>
2025-08-25 15:25:49 -0300

    Subindo novos exercicios

---
+++ exce01.py
@@ -0,0 +1,6 @@
+nome = input(f"Qual seu nome: ")
+cidade = input("De onde voce mora ")
+idade = int(input("Quantos anos voce tem?"))
+
+
+print(f"Prazer meu nome é {nome}, sou natural de {cidade}, e tenho {idade} anos")
